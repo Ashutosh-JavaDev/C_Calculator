f2c57c221488bf47d889e0b39e5a4eabe6c40a06
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-08 21:17:41 +0530

    C_Calculatorr

--- Calculator.c
+++ Calculator.c
@@ -1,6 +1,21 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 void addition();
 void substraction();
 void multipication();
 void division();
+int main()
+{
+    int choice;
+    while (1)
+    {
+        printf("Enter the Number what data  want to print: \n");
+        printf("1. Addition\n");
+        printf("1. Addition\n");
+        printf("1. Addition\n");
+        printf("1. Addition\n");
+        printf("1. Addition\n");
+        printf("1. Addition\n");
+        printf("1. Addition\n");
+    }
+}
